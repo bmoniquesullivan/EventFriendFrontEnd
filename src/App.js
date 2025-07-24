a0f57ca7eaf8eb86c954e34d18b5e7acd5660dfd
Elizxo <108903373+Elizxo@users.noreply.github.com>
2025-07-24 04:26:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import Layout
+// Layout
 import Layout from './components/Layout';
 
-// Import Pages
+// Pages
 import LoginPage from './pages/LoginPage';
 import EventFeedPage from './pages/EventFeedPage';
 import EventDetailPage from './pages/EventDetailPage';
 import DashboardPage from './pages/DashboardPage';
 import ProfilePage from './pages/ProfilePage';
+import ChatPg from './pages/ChatPage';  
 
 function App() {
   return (
     <Router>
-        <Routes>
-            {/* Routes with Navbar and Footer */}
-            <Route path="/events" element={<Layout><EventFeedPage /></Layout>} />
-            <Route path="/events/:id" element={<Layout><EventDetailPage /></Layout>} />
-            <Route path="/dashboard" element={<Layout><DashboardPage /></Layout>} />
-            <Route path="/profile" element={<Layout><ProfilePage /></Layout>} />
+      <Routes>
+        {/* Pages with Layout */}
+        <Route path="/events" element={<Layout><EventFeedPage /></Layout>} />
+        <Route path="/events/:id" element={<Layout><EventDetailPage /></Layout>} />
+        <Route path="/dashboard" element={<Layout><DashboardPage /></Layout>} />
+        <Route path="/profile" element={<Layout><ProfilePage /></Layout>} />
+        <Route path="/chat" element={<Layout><ChatPg /></Layout>} />  // ADDED CHAT PG
 
-            {/* Full-screen route (no Navbar/Footer) */}
-            <Route path="/" element={<LoginPage />} />
-        </Routes>
+        {/* Login page  */}
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
     </Router>
   );
 }
